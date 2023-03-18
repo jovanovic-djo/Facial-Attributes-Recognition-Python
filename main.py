1e9d7d48af7daa91874a08cae1c2215bae409ad0
Đorđe Jovanović <119633175+jovanovic-djo@users.noreply.github.com>
2023-03-18 23:24:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import cv2
+import os
+import pandas as pd
+from deepface import DeepFace
 
+#### Viewing results for recognition of distinct human face 
+# img = cv2.imread("faces/1 (1).jpeg")
+
+# result = DeepFace.analyze(img, actions=("gender", "age", "race", "emotion"))
+
+# print(result)
+
+### Further lines of code are for multiple data within directorium
