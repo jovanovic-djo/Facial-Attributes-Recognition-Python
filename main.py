25f6885fe28b7fdf2d656df93424da08b75eaa0f
Đorđe Jovanović <119633175+jovanovic-djo@users.noreply.github.com>
2023-03-19 10:29:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ from deepface import DeepFace
 # print(result)
 
 ### Further lines of code are for multiple data within directorium
+
+data = {
+    "Name": [],
+    "Age": [],
+    "Gender": [],
+    "Race": []
+}
+
+for file in os.listdir("testFaces"):
+    try:
+        result = DeepFace.analyze(cv2.imread(f"testFaces/{file}"), actions = ("age", "gender", "race"))
+        data["Name"].append(file.split(".")[0])
+        data["Age"].append(result[0]["age"])
+        data["Gender"].append(result[0]["dominant_gender"])
+        data["Race"].append(result[0]["dominant_race"])
+    except ValueError as e:
+        print(f"Error analyzing {file}: {str(e)}")
+df = pd.DataFrame(data)
+
+print(df)
+df.to_csv("people.csv")
+
+### For testing purposes, 
